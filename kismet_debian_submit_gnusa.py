86982d3056441ee2bd648929377d5ab5036ff73e
roohmadi <roohmadi@gmail.com>
2024-09-07 12:49:00 +0700

    Update kismet_debian_submit_gnusa.py

--- kismet_debian_submit_gnusa.py
+++ kismet_debian_submit_gnusa.py
@@ -2,11 +2,11 @@ import requests
 import re
 import numpy as np
 import uuid
-import time
+
 #import json
 from datetime import datetime
-
-device_id = "GAI-DENCITY.2408.00001"
+global device_id
+device_id = ""#"DENCITY-Playup.2409.00001"
 KISMET_USER = "USER_KISMET"
 KISMET_PASSWORD = "PASSWORD_KISMET"
 
@@ -19,18 +19,26 @@ api_key = "D80B96E4D7E4614D9926A0BAD9C9B6F2"         # Ganti dengan API key Anda
 headers1 = {
     "Authorization": f"Basic {api_key}"
 }
-headers = {"Content-Type": "application/json; charset=utf-8",'uspw': '{"user":"admin@playup.com","pass":"playup123"}'}
+headers_data = {"Content-Type": "application/json; charset=utf-8",'XA': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiJERU5DSVRZIiwiaWF0IjoxNzI1NTI3NzY0fQ.1X0pGlp5MEB72489yGXN2re9jTF9B6HyJuxE054Bcsk'}
+#headers_data = {"Content-Type": "application/json; charset=utf-8",'uspw': '{"user":"admin@playup.com","pass":"playup123"}'}
+headers_regis = {"Content-Type": "application/json; charset=utf-8"}
 # Endpoint untuk mendapatkan perangkat yang terdeteksi
 #DEVICES_ENDPOINT = "/devices/views/physdevices.json"
 DEVICES_ENDPOINT = "/devices/last-time/0/devices.json"
-# D80B96E4D7E4614D9926A0BAD9C9B6F2
-# URL PHP script
-PHP_URL = "https://produk-inovatif.com/latihan/wifi/submit_data.php"
-url = 'https://adm-iot.gnusa.id/service/device_transmit'
+url_data = 'https://adm-iot.gnusa.id/service/device_transmit'
+url_regis = 'https://adm-iot.gnusa.id/service/reg_mac_address'
+      #'https://adm-iot.gnusa.id/service/device_transmit'
 # Header untuk otorisasi
 headers1 = {
     "Authorization": f"Basic {api_key}"
 }
+
+def get_mac_address():
+    mac_address = uuid.getnode()
+    mac_address_id = (':'.join(['{:02x}'.format((mac_address >> elements) & 0xff) for elements in range(0,8*6,8)][::-1])).upper()
+    
+    
+    return mac_address_id
 
 def estimate_distance(power_received, params=None):
     """This function returns an estimated distance range
@@ -71,6 +79,32 @@ def estimate_distance(power_received, params=None):
 
     return (np.round(d_est,2), np.round(d_min,2), np.round(d_max,2))
 
+def get_device_id ():
+    global device_id
+    data_device ={
+    "api_key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiJERU5DSVRZIiwiaWF0IjoxNzI1NTI3NzY0fQ.1X0pGlp5MEB72489yGXN2re9jTF9B6HyJuxE054Bcsk",
+    "mac_address":get_mac_address(),
+    "device_type":"Mini PC"}
+    
+    try:
+        response = requests.post(url_regis, headers=headers_regis, json=data_device)
+        print("Status Code", response.status_code)
+        res_json = response.json()
+        print("JSON Response ", res_json)
+        device_id = res_json["data"]["device_id"]
+        #print(type(response.json()))
+        #print(res_json["data"]["device_id"])
+        #print(response)
+        
+        #response = requests.post(PHP_URL, data=payload)
+        #print(response)
+        #response.raise_for_status()
+        print(f"Data get device id successfully")
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to get device id")
+        return False
+    
 def get_devices():
     try:
         response = requests.get(f"{KISMET_URL}{DEVICES_ENDPOINT}", headers=headers1)
@@ -84,53 +118,14 @@ def get_devices():
         print(f"An error occurred: {e}")
         return None
 
-def submit_device_data_print(device):
-    mac_address = uuid.getnode()
-    mac_address_id = (':'.join(['{:02x}'.format((mac_address >> elements) & 0xff) for elements in range(0,8*6,8)][::-1])).upper()
-
-    #print('mac_address': device['kismet.device.base.macaddr')
-    #print("=" * 10)
-    print(f"Device Name: {device.get('kismet.device.base.name', 'N/A')}")
-    print(f"MAC Address: {device.get('kismet.device.base.macaddr', 'N/A')}")
-    print(f"Type: {device.get('kismet.device.base.type', 'N/A')}")
-    print(f"First Seen: {device.get('kismet.device.base.first_time', 'N/A')}")
-    print(f"Last Seen: {device.get('kismet.device.base.last_time', 'N/A')}")
-    
-    first_stamp = int(f"{device.get('kismet.device.base.first_time', 'N/A')}")
-    print("----")
-    print(first_stamp)
-    print(datetime.fromtimestamp(first_stamp))
-    print("====")
-    
-    #print(type({device.get['kismet.device.base.signal']['kismet.common.signal.last_signal']}))
-    str_sig = f"{device.get('kismet.device.base.signal','N/A')}"
-    if len(str_sig) > 0:
-        sig = re.split(",",re.split("':", str_sig)[4])[0]
-        if len(sig)>0:
-            int_sig = int(sig)
-            distance,min_dist, max_dist = estimate_distance(int_sig)
-    print('--==--')
-    print(str_sig)
-    print(sig)
-    print(distance)
-    #print(x2)
-    print('--==--')
-    #sig_dict = json.loads(str_sig)
-    #print(type(dict_sig))
-    #print(dict_sig)
-    print(f"Signal Strength: {sig}")
-    print(f"Manufacturer: {device.get('kismet.device.base.manuf', 'N/A')}")
-    print(f"Frequency: {device.get('kismet.device.base.frequency', 'N/A')}")
-    print("-" * 40)
-    
 def submit_device_data(device):
     mac_address = uuid.getnode()
     mac_address_id = (':'.join(['{:02x}'.format((mac_address >> elements) & 0xff) for elements in range(0,8*6,8)][::-1])).upper()
 
     str_sig = f"{device.get('kismet.device.base.signal','N/A')}"
-    #print(str_sig)
-    #print(len(str_sig))
-    if len(str_sig) > 0:
+    print(str_sig)
+    print(len(str_sig))
+    if len(str_sig) > 3:
         sig = re.split(",",re.split("':", str_sig)[4])[0]
         last_noise = re.split(",",re.split("':", str_sig)[3])[0]
         min_noise = re.split(",",re.split("':", str_sig)[5])[0]
@@ -147,6 +142,7 @@ def submit_device_data(device):
     #dt = datetime.fromtimestamp(ts)
     
     data = {
+    "api_key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdfaWQiOiJERU5DSVRZIiwiaWF0IjoxNzI1NTI3NzY0fQ.1X0pGlp5MEB72489yGXN2re9jTF9B6HyJuxE054Bcsk",
     "device_id": device_id,
     "raw_data" : [{
     "type":device.get('kismet.device.base.type', 'N/A'),
@@ -189,7 +185,7 @@ def submit_device_data(device):
         print(data)
         #print(headers)
         
-        response = requests.post(url, headers=headers, json=data)
+        response = requests.post(url_data, headers=headers_data, json=data)
         print("Status Code", response.status_code)
         print("JSON Response ", response.json())
         print(response)
@@ -202,16 +198,16 @@ def submit_device_data(device):
         print(f"Failed to submit data for MAC: {payload['mac_address']} - {e}")
 
 def main():
-    devices_data = get_devices()
-    #print(devices_data)
-    if devices_data:
-        for device in devices_data:
-            #print("----")
-            #print(device)
-            submit_device_data(device)
-            #submit_device_data_print(device)
+    #print(get_device_id())
+    #print(device_id)
+    if (get_device_id()):
+        devices_data = get_devices()
+        if devices_data:
+            for device in devices_data:
+                submit_device_data(device)
+      
 
 if __name__ == "__main__":
     while True:
         main()
-        time.sleep(5)
+        time.sleep(10)
